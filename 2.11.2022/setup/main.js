6235973e148736afbb80add6a58852553828cc08
HauntedMilkshake <spaszahov@gmail.com>
2022-11-15 22:49:30 +0200

    homework with some explanations on .anchor

--- 2.11.2022/setup/main.js
+++ 2.11.2022/setup/main.js
@@ -1,6 +1,6 @@
 'use strict'
 //striketn mod v js 
-const Game = new Phaser.Game(800, 800, Phaser.AUTO, '', { preload, create, update })
+const Game = new Phaser.Game(window.innerWidth, window.innerHeight, Phaser.AUTO, '', { preload, create, update })
 //preload create i update sa osnovni metodi ako sa dadeni v konstruktora znachi che trqbva da gi definirame
 //preload e kato loadMedia v sdl
 function preload() {
@@ -8,32 +8,14 @@ Game.load.image("diuner", "images/pileshki_duner_grand.png")
 Game.load.image("background", "nebe.jpg")
 //tova diuner e kliuch i moje da e kakvoto si iskame
 }
-// ne znam dali chetete komentari 
-// no ako chetete 
-/*
-░▄▀▄▀▀▀▀▄▀▄░░░░░░░░░
-░█░░░░░░░░▀▄░░░░░░▄░
-█░░▀░░▀░░░░░▀▄▄░░█░█
-█░▄░█▀░▄░░░░░░░▀▀░░█
-█░░▀▀▀▀░░░░░░░░░░░░█
-█░░░░░░░░░░░░░░░░░░█
-█░░░░░░░░░░░░░░░░░░█
-░█░░▄▄░░▄▄▄▄░░▄▄░░█░
-░█░▄▀█░▄▀░░█░▄▀█░▄▀░
-░░▀░░░▀░░░░░▀░░░▀░░░
-totally accurate representation of kashmir 1:1 scale 
 
-*/
 function create() {
     Game.add.sprite(0, 0,"background");
-    Game.add.sprite(400, 400, "diuner").anchor.setTo(0.5);
-    Game.add.sprite(400, 400, "diuner").anchor.setTo(1);
-    Game.add.sprite(400, 400, "diuner").anchor.setTo(0);
-    Game.add.sprite(400, 400, "diuner").anchor.setTo(0, 1);
-    Game.add.sprite(400, 400, "diuner").anchor.setTo(1, 0);
-    
-    
-    
+    Game.add.sprite(Game.width / 2, Game.height / 2, "diuner").anchor.setTo(0.5);
+    Game.add.sprite(Game.width / 2, Game.height / 2, "diuner").anchor.setTo(1);
+    Game.add.sprite(Game.width / 2, Game.height / 2, "diuner").anchor.setTo(0);
+    Game.add.sprite(Game.width / 2, Game.height / 2, "diuner").anchor.setTo(0, 1);
+    Game.add.sprite(Game.width / 2, Game.height / 2, "diuner").anchor.setTo(1, 0);
 }
 
 function update() {
--- 9.11.2022/main.js
+++ 9.11.2022/main.js
@@ -1,12 +1,39 @@
 'use strict'
+
 const Game = new Phaser.Game(window.innerWidth, window.innerHeight, Phaser.AUTO, "", { preload, create })
-console.log(document); // document e html faila ni 
+//console.log(document); // document e html faila ni 
 function preload() {
     Game.load.image("background", "background.png");
+    Game.load.image("dumbbell", "dumbbell.png");
 }
 
 function create() {
-    
+    Game.add.sprite(0, 0, "background");
+
+    let dumbbellTopLeft = Game.add.sprite(0, 0, "dumbbell");
+    dumbbellTopLeft.scale.setTo(0.5);
+
+    let dumbellMiddle = Game.add.sprite(Game.width / 2, Game.height / 2, "dumbbell");
+    dumbellMiddle.scale.setTo(1);
+    dumbellMiddle.anchor.setTo(0.5);
+
+    let dumbellTopRight = Game.add.sprite(Game.width, 0, "dumbbell");
+    dumbellTopRight.scale.setTo(0.5);
+    dumbellTopRight.anchor.setTo(1, 0);
+
+    let dumbellBottomLeft = Game.add.sprite(0, Game.height, "dumbbell");
+    dumbellBottomLeft.scale.setTo(0.5);
+    dumbellBottomLeft.anchor.setTo(0, 1);
+
+    let dumbellBottomRight = Game.add.sprite(Game.width, Game.height, "dumbbell");
+    dumbellBottomRight.scale.setTo(0.5);
+    dumbellBottomRight.anchor.setTo(1,1);
+    //.anchoir.setTo() raboti po sledniq nachin: priema 2 parametura za x i y. Te ni pokazvat sprqmo koq tochka ot snimkata shte q risuvame
+    // 0,0 risuvame q ot gorniq lqv ugul 
+    // 1,0 premestili sme do kraq na snimkata po x horizontal, no ne na y i q risuvame ot gorniq desen ugul 
+    // 0,1 premestili sme do kraq na snimkata po y vertikal, no ne na x i q risuvame ot dolniq lqv ugul 
+    // 0.5 , 0.5 premestili sme do sredata na snimkata i q risuvame ot tam 
+    /*
     let nebeBottomRight = Game.add.sprite(Game.width / 2, Game.height / 2, "background"); // responsive design 
     nebeBottomRight.scale.setTo(0.25);
     nebeBottomRight.anchor.setTo(1);
@@ -17,12 +44,13 @@ function create() {
     
     let nebe2TopRight = Game.add.sprite(Game.width / 2, Game.height / 2, "background");
     nebe2TopRight.scale.setTo(0.25);
-    nebe2TopRight.anchor.setTo(0, 1);//tochkata sprqmo koqto pozicionirame 0,0 gore lqvo na snimkata 1,1 dolu dqsno 0,1 gore dqsno 1,0 dolu lqvo
+    nebe2TopRight.anchor.setTo(0, 1);//tochkata sprqmo koqto pozicionirame 0,0 gore lqvo na snimkata 1,1 dolu dqsno 0,1 dolu lqvo 1,0 gore dqsno
+    //tochkata sprqmo koqto pozicionirame
     
     let nebe3TopLeft = Game.add.sprite(Game.width / 2, Game.height / 2, "background");
     nebe3TopLeft.scale.setTo(0.25);   
     nebe3TopLeft.anchor.setTo(0, 0);
-    
+    */
     
    /*
    Game.add.sprite(0, 0, "background").scale.setTo(0.25);
