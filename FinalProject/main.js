0ef39b3d8e4737c49cfa55165d5bef0f62820f3a
HauntedMilkshake <spaszahov@gmail.com>
2023-03-06 22:57:43 +0200

    ghost mode is almost done

--- FinalProject/main.js
+++ FinalProject/main.js
@@ -1,26 +1,33 @@
 'use strict'
-const Game = new Phaser.Game(400, 400, Phaser.AUTO, "", { preload, create, update })
+const Game = new Phaser.Game(400, 400, Phaser.AUTO, "", { preload, create, update });
 let player;
+let ghost; 
+let hideGhostX = -100;
+let hideGhostY = 0;
+let playerSpeed = 50;
+let ghostSpeed = 35;
 let playerState = true;
-let button_swap, button_a, button_d, button_w;
+let button_swap, button_a, button_d, button_w, button_s;
 // true - human form 
 // false - ghost form
 // druga promenliva za duha sprqmo playerState shte mestq ili duha ili playera 
 function preload() {
     //("ghostIdle", "assets\ghost\idle.png");
     Game.load.spritesheet("human", "assets/player/spritesheet.png", 288  / 9, 32);
+    Game.load.image("ghost", "assets/ghost/idle.png");
 }
 
 function create() {
     createKeys();
     createPlayer();
+    createGhost();
 }
 
 function createPlayer() {
     player = Game.add.sprite(64, 200, "human");
 
     player.anchor.setTo(0.5);
-    player.scale.setTo(1);
+    player.scale.setTo(2);
 
     player.animations.add("idle", [0 , 1], 1, true);
     player.animations.add("run", [2, 3, 4, 5, 6, 7, 8], 15, true);
@@ -29,41 +36,83 @@ function createPlayer() {
 
 }
 
+function createGhost(){
+    ghost = Game.add.sprite(hideGhostX, hideGhostY, "ghost");
+    ghost.anchor.setTo(0.5);
+    ghost.scale.setTo(2);
+    Game.physics.enable(ghost, Phaser.Physics.ARCADE);
+    
+}
+
 function createKeys() {
     button_w = Game.input.keyboard.addKey(Phaser.Keyboard.W);
     button_a = Game.input.keyboard.addKey(Phaser.Keyboard.A);
     button_d = Game.input.keyboard.addKey(Phaser.Keyboard.D);
+    button_s = Game.input.keyboard.addKey(Phaser.Keyboard.S);
     button_swap = Game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
 }
 
 
 function update() {
     movePlayer();
+    switchToGhost();
 }
 
 function movePlayer() {
     if(playerState){
    
         if(button_a.isDown){
-            player.body.velocity.x = -50;
+            player.body.velocity.x = -playerSpeed;
             player.animations.play("run");
+            player.scale.setTo(-2, 2);  
+
         }else if(button_d.isDown){
-            player.body.velocity.x = +50;
-            player.scale.setTo(-1, 1);
+            player.body.velocity.x = +playerSpeed;
             player.animations.play("run");
+            player.scale.setTo(2, 2);
         }
         
-        if(!button_a.isDown || !button_d.isDown){
+        if(!(button_a.isDown || button_d.isDown)){
             player.animations.play("idle");
+            player.body.velocity.x = 0;
+            player.scale.setTo(2);
         }
-    }else if(!playerState){
-        // tuka trqbva da go izmislish i za space - a 
-    }
 
+    }else if(!playerState){
+       
+        
+        if(button_a.isDown){
+            ghost.body.velocity.x = -ghostSpeed;
+
+        }else if(button_d.isDown){
+            ghost.body.velocity.x = +ghostSpeed;
+
+        }else if(button_w.isDown){
+            ghost.body.velocity.y = -ghostSpeed;
+
+        }else if(button_s.isDown){
+            ghost.body.velocity.y = +ghostSpeed;
+
+        }else if(!(button_a.isDown || button_d.isDown || button_w.isDown || button_s.isDown)){
+            ghost.body.velocity.x = 0;
+            ghost.body.velocity.y = 0;
+        }
+    }
 }
 
 function switchToGhost() {
-    if(button_swap.isDown){
-        playerState = false;
+    console.log(button_swap.onPres);
+    if(button_swap.onPress){
+
+        console.log("1: " + playerState);
+        playerState = !(playerState);
+        ghost.x = player.x + 32;
+        ghost.y = player.y; 
+        console.log("2: " + playerState);
+
+        if(playerState){
+            ghost.x = hideGhostX;
+            ghost.y = hideGhostY;
+        }
     }
 }
